fb735f5bf44999f48023422b197300ba50d56eb5
Dino <dpasic8@gmail.com>
2022-12-03 20:02:16 +0100

    methods should work, but as we heavily rely on these methods working we should write some tests

--- src/main/information_scent/IUNIS.py
+++ src/main/information_scent/IUNIS.py
@@ -15,17 +15,22 @@ tfidf_keywords.columns = ["id", "keyword"]
 # Loading in the adjacency matrix
 # First column and row are data immediately, so access directly by page id
 # If row(page_id_1) leads to column(page_id_2) = 1 else = 0
-adjacency_matrix = pd.read_csv("../data/matrices/adjacency_matrix.csv")
+adjacency_matrix = pd.read_csv("../data/matrices/adjacency_matrix.csv", header = None)
 
 
 def get_id_by_keyword(keyword):
     return tfidf_keywords.id.iloc[tfidf_keywords[tfidf_keywords.keyword == keyword].index].values[0]
+
 
 # Method to get the tfidf value of a keyword in a page
 def get_tf_idf_value(page_id, keyword):
     keyword_id = get_id_by_keyword(keyword)
     return tfidf.iloc[page_id,keyword_id+1]
 
-# Not sure if this indexing is correct, TODO: Matthew please confirm that the first row and first column in the adj matrix file are useless
+
+# Method to get the adjacency value from two page ids
 def get_adjacency_value(page_id_1, page_id_2):
-    return adjacency_matrix.iloc[page_id_1,page_id_2+1]==1
+    return adjacency_matrix.iloc[page_id_1,page_id_2]==1
+
+
+print(get_adjacency_value(7,3))
