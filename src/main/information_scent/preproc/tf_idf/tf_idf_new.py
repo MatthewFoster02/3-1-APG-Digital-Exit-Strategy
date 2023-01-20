0103c6123c0052998768f948f6e8083957c36678
Dino <dpasic8@gmail.com>
2023-01-20 19:57:47 +0100

    fixed FR FR

--- src/main/information_scent/preproc/tf_idf/tf_idf_new.py
+++ src/main/information_scent/preproc/tf_idf/tf_idf_new.py
@@ -3,22 +3,25 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from matplotlib import pyplot as plt
 from collections import Counter
-
+import re
 # Change this variable and the path in the else statement below.
 laurence = False
 path = None
 if laurence:
     path = "C:/Users/laure/OneDrive/Documenten/Project 3.1 APG Files/"
 else:
-    path = "C:/Users/01din/Desktop/Data APG/scrape_results/"
+    path = "C:/Users/01din/Desktop/Data APG/FIXED_SCRAPES/"
 
 
-corpus = pd.read_csv(path + "final.csv")
+corpus = pd.read_csv(path + "url_references_with_paragraphs_no_english.csv")
+
+
+
 # "public_and_private_paragraphs_none_filtered.csv"
 df = pd.DataFrame({'text':corpus.paragraphs.tolist()})
 c = Counter(df.text.str.split().explode())
 df = df.text.apply(lambda x: ' '.join(w for w in x.split() if c[w] >= 5).strip())
-
+c_list = df.values.tolist()
 
 vectorizer = TfidfVectorizer(max_df=1.0)
 vec_trained = vectorizer.fit_transform(df)
