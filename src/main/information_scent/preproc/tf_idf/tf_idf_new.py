73a32355f46c5dcdab52b658b5b8c43a3483f366
Dino <dpasic8@gmail.com>
2023-01-20 13:38:51 +0100

    fixed defo

--- src/main/information_scent/preproc/tf_idf/tf_idf_new.py
+++ src/main/information_scent/preproc/tf_idf/tf_idf_new.py
@@ -2,10 +2,7 @@ import string
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from matplotlib import pyplot as plt
-import nltk
 from collections import Counter
-
-nltk.download('stopwords')
 
 # Change this variable and the path in the else statement below.
 laurence = False
@@ -16,18 +13,18 @@ else:
     path = "C:/Users/01din/Desktop/Data APG/scrape_results/"
 
 
-corpus = pd.read_csv(path + "public_and_private_sentences_none_filtered.csv")
-
-df = pd.DataFrame({'text':corpus.sentences.tolist()})
+corpus = pd.read_csv(path + "final.csv")
+# "public_and_private_paragraphs_none_filtered.csv"
+df = pd.DataFrame({'text':corpus.paragraphs.tolist()})
 c = Counter(df.text.str.split().explode())
 df = df.text.apply(lambda x: ' '.join(w for w in x.split() if c[w] >= 5).strip())
 
-final_corpus = []
-for i in range(len(df)):
-    sentence = df.iloc[i]
-    final_corpus[corpus.document.iloc[i]-1] = final_corpus[corpus.document.iloc[i]-1] + " " + sentence
 
 vectorizer = TfidfVectorizer(max_df=1.0)
 vec_trained = vectorizer.fit_transform(df)
 
-keywords_tf_idf = pd.DataFrame(vectorizer.get_feature_names_out()).values.tolist()
+
+pd.DataFrame(vec_trained.todense()).to_csv('C:/Users/01din/Desktop/data APG/tfidf/tf_idf_new.csv')
+pd.DataFrame(vectorizer.get_feature_names_out()).to_csv('C:/Users/01din/Desktop/data APG/tfidf/tf_idf_keywords_new.csv')
+
+print(len(vectorizer.get_feature_names_out()))
